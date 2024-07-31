e0e6c561e93a9516ccaf72cfc35b76cf35c6a3ba
Nick Canarelli <nick@nickcanarelli.com>
2024-07-31 11:13:55 -0400

    initial commit

---
+++ prettier.config.js
@@ -0,0 +1,3 @@
+import { frontend } from './src/index.js'
+
+export default frontend
---
+++ src/index.js
@@ -0,0 +1,25 @@
+/** Prettier configuration tailored to backend preferences */
+export const backend = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  endOfLine: 'auto',
+  jsxSingleQuote: true,
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+}
+
+/** Prettier configuration tailored to frontend preferences */
+export const frontend = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  endOfLine: 'auto',
+  jsxSingleQuote: true,
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+}
